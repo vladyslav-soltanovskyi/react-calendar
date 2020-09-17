3fe3e4c4013b2c32fe1f9f08973d114e37e76f12
Andrii <amudrevskiy@gmail.com>
2020-09-17 16:14:31 +0300

    upd

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,46 +3,41 @@ import React, { Component } from 'react';
 import './modal.scss';
 
 class Modal extends Component {
-    state = {
-        id: '',
-        date: '',
-        startTime: '',
-        finishTime: '',
-        title: '',
-        description: ''
-    }
 
-    handleChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        })
-    }
-
-
-    onSubmit = event => {
-        debugger;
-        event.preventDefault();
-        this.props.handleSubmit(this.state);
-    };
 
     render() {
-        const { hideModalHandler } = this.props;
 
         return (
             <div className="modal overlay">
                 <div className="modal__content">
                     <div className="create-event">
-                        <button className="create-event__close-btn" onClick={hideModalHandler}>+</button>
-                        <form className="event-form" onSubmit={this.onSubmit}>
-                            <input type="text" name="title" placeholder="Title" className="event-form__field" onChange={this.handleChange} />
+                        <button className="create-event__close-btn">+</button>
+                        <form className="event-form">
+                            <input type="text"
+                                name="title"
+                                placeholder="Title"
+                                className="event-form__field"
+                            />
                             <div className="event-form__time">
-                                <input type="date" name="date" className="event-form__field" onChange={this.handleChange} />
-                                <input type="time" name="startTime" className="event-form__field" onChange={this.handleChange} />
+                                <input type="date"
+                                    name="date"
+                                    className="event-form__field"
+                                />
+                                <input type="time"
+                                    name="startTime"
+                                    className="event-form__field"
+                                    onChange={this.handleChange}
+                                />
                                 <span>-</span>
-                                <input type="time" name="endTime" className="event-form__field" onChange={this.handleChange} />
+                                <input type="time"
+                                    name="endTime"
+                                    className="event-form__field"
+                                />
                             </div>
-                            <textarea name="description" placeholder="Description" className="event-form__field" onChange={this.handleChange}></textarea>
+                            <textarea name="description"
+                                placeholder="Description"
+                                className="event-form__field">
+                            </textarea>
                             <button type="submit" className="event-form__submit-btn">Create</button>
                         </form>
                     </div>
