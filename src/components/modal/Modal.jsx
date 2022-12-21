bdcc972cd037971bdede65d6145452272c992af3
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-21 13:20:28 +0100

    Refactor existing code - use hooks: useState, useEffect

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './modal.scss';
 
-class Modal extends Component {
-  render() {
-    return (
-      <div className="modal overlay">
-        <div className="modal__content">
-          <div className="create-event">
-            <button className="create-event__close-btn">+</button>
-            <form className="event-form">
+const Modal = () => {
+  return (
+    <div className="modal overlay">
+      <div className="modal__content">
+        <div className="create-event">
+          <button className="create-event__close-btn">+</button>
+          <form className="event-form">
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              className="event-form__field"
+            />
+            <div className="event-form__time">
+              <input type="date" name="date" className="event-form__field" />
               <input
-                type="text"
-                name="title"
-                placeholder="Title"
+                type="time"
+                name="startTime"
                 className="event-form__field"
               />
-              <div className="event-form__time">
-                <input type="date" name="date" className="event-form__field" />
-                <input
-                  type="time"
-                  name="startTime"
-                  className="event-form__field"
-                  onChange={this.handleChange}
-                />
-                <span>-</span>
-                <input
-                  type="time"
-                  name="endTime"
-                  className="event-form__field"
-                />
-              </div>
-              <textarea
-                name="description"
-                placeholder="Description"
+              <span>-</span>
+              <input
+                type="time"
+                name="endTime"
                 className="event-form__field"
-              ></textarea>
-              <button type="submit" className="event-form__submit-btn">
-                Create
-              </button>
-            </form>
-          </div>
+              />
+            </div>
+            <textarea
+              name="description"
+              placeholder="Description"
+              className="event-form__field"
+            ></textarea>
+            <button type="submit" className="event-form__submit-btn">
+              Create
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Modal;
