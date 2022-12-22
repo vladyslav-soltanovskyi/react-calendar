5c669ac309f8d0fc11bea90b3937e4fe06949a54
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-22 22:33:49 +0100

    added for navigation display of the current day, previous days and future days

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import propTypes from "prop-types";
+import cn from "classnames";
 
-import { days } from '../../utils/dateUtils.js';
+import { days, getDateObj } from '../../utils/dateUtils.js';
 
 const Navigation = ({ weekDates }) => {
+  const currentDate = new Date();
+  const currentDateObj = getDateObj(currentDate);
   return (
     <header className="calendar__header">
       {weekDates.map((dayDate) => {
-        const uniqKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}`;
+        const dayDateObj = getDateObj(dayDate);
+        const uniqKey = `${dayDateObj.year}-${dayDateObj.month}-${dayDateObj.day}`;
+        
+        const isCurrentYear = dayDateObj.year === currentDateObj.year;
+        const isCurrentMonth = dayDateObj.month === currentDateObj.month;
+        const isCurrentDay = dayDateObj.day === currentDateObj.day;
+        const isCurrentDate = isCurrentYear && isCurrentMonth && isCurrentDay;
+        const isPastDay = (currentDate.getTime() > dayDate.getTime()) && !isCurrentDate; 
+
         return (
           <div
             className="calendar__day-label day-label"
             key={uniqKey}
           >
-            <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-            <span className="day-label__day-number">{dayDate.getDate()}</span>
+            <span
+              className={cn("day-label__day-name", {
+                "day-label__day-name_today": isCurrentDate
+              })}
+            >
+              {days[dayDate.getDay()]}</span>
+            <span
+              className={cn("day-label__day-number", {
+                "day-label__day-number_today": isCurrentDate,
+                "day-label__day-number_past": isPastDay
+              })}
+            >
+              {dayDateObj.day}</span>
           </div>
         )
       })}
