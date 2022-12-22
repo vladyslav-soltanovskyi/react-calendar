728c9730d721b00528089a06e7290fb1fd05c67e
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-22 21:35:27 +0100

    Draw red line for current day and time

--- src/components/time-line/TimeLine.jsx
+++ src/components/time-line/TimeLine.jsx
@@ -52,36 +52,3 @@ TimeLine.propTypes = {
 }
 
 export default TimeLine;
-
-// import { getDateObj } from '../common/time.utils.js';
-
-// let timerId;
-
-// const createTimeLine = (offsetTop) => {
-//   const timeLine = document.createElement('div');
-//   timeLine.classList.add('time-line');
-//   timeLine.style.top = offsetTop + "px";
-//   return timeLine;
-// }
-
-// const removeTimerForTimeLine = () => {
-//   const timeLine = document.querySelector('.time-line');
-//   timeLine?.remove();
-//   clearInterval(timerId);
-// }
-
-// export const renderTimeLine = () => {
-//   removeTimerForTimeLine();
-
-//   const { minutes, seconds, hours, day } = getDateObj(new Date());
-//   const delayForNextRender = minute - (seconds * 1000);
-  
-//   const calendarTimeSlot = document.querySelector(
-//     `[data-day="${day}"] [data-time="${hours}"]`
-//   );
-
-//   const timeLine = createTimeLine(minutes);
-//   calendarTimeSlot && calendarTimeSlot.append(timeLine);
-  
-//   timerId = setInterval(renderTimeLine, delayForNextRender);
-// };
